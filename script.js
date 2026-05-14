1c8848b9db6cca816e0498d7aa345616a0b14027
Bhav Simar <bhavsimar39@gmail.com>
2026-05-14 19:49:21 +0530

    chnages

--- script.js
+++ script.js
@@ -12,23 +12,17 @@ window.addEventListener("scroll", () => {
     let current = "";
 
     sections.forEach((section) => {
-
         const sectionTop = section.offsetTop;
-
         if(pageYOffset >= sectionTop - 200){
             current = section.getAttribute("id");
         }
-
     });
 
     navLinks.forEach((link) => {
-
         link.classList.remove("active");
-
         if(link.getAttribute("href") === "#" + current){
             link.classList.add("active");
         }
-
     });
 
 });
@@ -36,34 +30,25 @@ window.addEventListener("scroll", () => {
 // ================= SCROLL ANIMATION =================
 
 const cards = document.querySelectorAll(
-".skill-card, .project-card"
+    ".skill-card, .project-card, .cert-card, .stat"
 );
 
 window.addEventListener("scroll", () => {
-
     cards.forEach((card) => {
-
         const cardTop = card.getBoundingClientRect().top;
-
         if(cardTop < window.innerHeight - 100){
-
             card.style.opacity = "1";
             card.style.transform = "translateY(0px)";
-
         }
-
     });
-
 });
 
 // ================= INITIAL CARD STYLE =================
 
 cards.forEach((card) => {
-
     card.style.opacity = "0";
     card.style.transform = "translateY(50px)";
     card.style.transition = "0.6s";
-
 });
 
 // ================= MOBILE MENU =================
@@ -73,38 +58,26 @@ const navMenu = document.querySelector(".nav-links");
 const navItems = document.querySelectorAll(".nav-links a");
 
 menuBtn.addEventListener("click", () => {
-
     navMenu.classList.toggle("active");
-
 });
-
-// CLOSE MENU AFTER CLICK
 
 navItems.forEach((item) => {
-
     item.addEventListener("click", () => {
-
         navMenu.classList.remove("active");
-
     });
-
 });
+
 // ================= TYPING ANIMATION =================
 
 var typed = new Typed(".typing", {
-
     strings: [
-
         "MERN Stack Developer",
-        "Frontend Developer",
+        "Full Stack Developer",
         "AI Enthusiast",
-        "Web Designer"
-
+        "Frontend Developer"
     ],
-
     typeSpeed: 80,
     backSpeed: 50,
     backDelay: 1500,
     loop: true
-
 });
